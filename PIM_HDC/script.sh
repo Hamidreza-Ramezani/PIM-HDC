08be1f8790a17785218b7d9f037ca02aa56acf71
hamidreza-ramezani <hamid.ramezani1375@gmail.com>
2024-08-09 20:08:08 -0700

    minor

--- PIM_HDC/script.sh
+++ PIM_HDC/script.sh
@@ -1,15 +1,38 @@
 #! /usr/sh
 
+dataset_path="/data/hamid/hdc-dataset"
 
-make clean >/dev/null
-make NR_DPUS=16 NR_TASKLETS=7 SHOW_DPU_LOGS=0 >/dev/null
-sudo ./pim_hdc -i data/tiny-data.bin -t
 
-make clean >/dev/null
-make NR_DPUS=256 NR_TASKLETS=14 SHOW_DPU_LOGS=0 >/dev/null
-sudo ./pim_hdc -i data/large-data.bin -t
+#make clean >/dev/null
+#make NR_DPUS=12 NR_TASKLETS=15 SHOW_DPU_LOGS=0 >/dev/null
+#sudo ./pim_hdc -i $dataset_path/tiny-data.bin -t
+#
+#make clean >/dev/null
+#make NR_DPUS=256 NR_TASKLETS=18 SHOW_DPU_LOGS=0 >/dev/null
+#sudo ./pim_hdc -i $dataset_path/large-data.bin -t
+#
+#make clean >/dev/null
+#make  SAMPLES_IN_MRAM=1 HDC_MAX_INPUT=960 NR_DPUS=512 NR_TASKLETS=18 SHOW_DPU_LOGS=0 >/dev/null
+#sudo ./pim_hdc -i $dataset_path/large8-data.bin -t
+#
+#
+#make clean >/dev/null
+#make  SAMPLES_IN_MRAM=1 HDC_MAX_INPUT=1900 NR_DPUS=1024 NR_TASKLETS=18 SHOW_DPU_LOGS=0 >/dev/null
+#sudo ./pim_hdc -i $dataset_path/large32-data.bin -t
+#
+#
+#make clean >/dev/null
+#make  SAMPLES_IN_MRAM=1 HDC_MAX_INPUT=6640 NR_DPUS=2304 NR_TASKLETS=18 SHOW_DPU_LOGS=0 >/dev/null
+#sudo ./pim_hdc -i $dataset_path/large256-data.bin -t
 
-make clean >/dev/null
-make NR_DPUS=512 NR_TASKLETS=12 SHOW_DPU_LOGS=0 >/dev/null
-sudo ./pim_hdc -i data/large8-data.bin -t
+
+#make clean >/dev/null
+#make  SAMPLES_IN_MRAM=1 HDC_MAX_INPUT=26480 NR_DPUS=2304 NR_TASKLETS=18 SHOW_DPU_LOGS=0 >/dev/null
+#sudo ./pim_hdc -i $dataset_path/large1024-data.bin -t
+
+
+#make clean >/dev/null
+#make  SAMPLES_IN_MRAM=1 HDC_MAX_INPUT=52940 NR_DPUS=2304 NR_TASKLETS=18 SHOW_DPU_LOGS=0 >/dev/null
+#sudo ./pim_hdc -i $dataset_path/large2048-data.bin -t
+
 
