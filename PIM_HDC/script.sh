bdf7b93c764320c85a05f35876ddb248962d5866
hamidreza-ramezani <hamid.ramezani1375@gmail.com>
2024-08-12 15:51:30 -0700

    minor

--- PIM_HDC/script.sh
+++ PIM_HDC/script.sh
@@ -36,3 +36,24 @@ dataset_path="/data/hamid/hdc-dataset"
 #sudo ./pim_hdc -i $dataset_path/large2048-data.bin -t
 
 
+make clean >/dev/null
+make NR_DPUS=12 NR_TASKLETS=15 SHOW_DPU_LOGS=0 >/dev/null
+sudo ./pim_hdc -i $dataset_path/tiny-data.bin
+
+make clean >/dev/null
+make NR_DPUS=256 NR_TASKLETS=18 SHOW_DPU_LOGS=0 >/dev/null
+sudo ./pim_hdc -i $dataset_path/large-data.bin
+
+make clean >/dev/null
+make  SAMPLES_IN_MRAM=1 HDC_MAX_INPUT=960 NR_DPUS=512 NR_TASKLETS=18 SHOW_DPU_LOGS=0 >/dev/null
+sudo ./pim_hdc -i $dataset_path/large8-data.bin
+
+
+make clean >/dev/null
+make  SAMPLES_IN_MRAM=1 HDC_MAX_INPUT=1900 NR_DPUS=1024 NR_TASKLETS=18 SHOW_DPU_LOGS=0 >/dev/null
+sudo ./pim_hdc -i $dataset_path/large32-data.bin
+
+
+
+
+
