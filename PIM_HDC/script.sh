8c69a4cafb2a60b4cc26636ead57cfc4a8a61dd1
hamidreza-ramezani <hamid.ramezani1375@gmail.com>
2024-09-13 00:01:09 -0700

    port to cortes

--- PIM_HDC/script.sh
+++ PIM_HDC/script.sh
@@ -5,23 +5,23 @@ dataset_path="/data/hamid/hdc-dataset"
 
 make clean >/dev/null
 make NR_DPUS=12 NR_TASKLETS=15 SHOW_DPU_LOGS=0 >/dev/null
-sudo ./pim_hdc -i $dataset_path/tiny-data.bin -t
+sudo ./pim_hdc -i $dataset_path/tiny-data.bin 
 
 make clean >/dev/null
 make NR_DPUS=256 NR_TASKLETS=18 SHOW_DPU_LOGS=0 >/dev/null
-sudo ./pim_hdc -i $dataset_path/large-data.bin -t
+sudo ./pim_hdc -i $dataset_path/large-data.bin 
 
 make clean >/dev/null
 make  SAMPLES_IN_MRAM=1 HDC_MAX_INPUT=960 NR_DPUS=512 NR_TASKLETS=18 SHOW_DPU_LOGS=0 >/dev/null
-sudo ./pim_hdc -i $dataset_path/large8-data.bin -t
+sudo ./pim_hdc -i $dataset_path/large8-data.bin 
 
 make clean >/dev/null
 make  SAMPLES_IN_MRAM=1 HDC_MAX_INPUT=1900 NR_DPUS=1024 NR_TASKLETS=18 SHOW_DPU_LOGS=0 >/dev/null
-sudo ./pim_hdc -i $dataset_path/large32-data.bin -t
+sudo ./pim_hdc -i $dataset_path/large32-data.bin 
 
 make clean >/dev/null
 make  SAMPLES_IN_MRAM=1 HDC_MAX_INPUT=6640 NR_DPUS=2304 NR_TASKLETS=18 SHOW_DPU_LOGS=0 >/dev/null
-sudo ./pim_hdc -i $dataset_path/large256-data.bin -t
+sudo ./pim_hdc -i $dataset_path/large256-data.bin 
 
 
 #make clean >/dev/null
